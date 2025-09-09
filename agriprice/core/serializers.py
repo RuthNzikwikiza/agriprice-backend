0fa67c3a9a9f90da19fb518c518c517ce5d3c617
Nice-91 <iradukundanice91@gmail.com>
2025-09-08 21:05:24 -0700

    Initial setup of UserProfile and Product models with API endpoints

---
+++ agriprice/core/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import UserProfile, Product
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
--- agriprice/core/views.py
+++ agriprice/core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import UserProfile, Product
+from .serializers import UserProfileSerializer, ProductSerializer
 
-# Create your views here.
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
