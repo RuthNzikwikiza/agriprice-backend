bd8b7f1a54296bba54c8f4068dd26b8f035e599e
Nice-91 <iradukundanice91@gmail.com>
2025-09-13 09:43:01 -0700

    added changes

--- agriprice/agriprice/urls.py
+++ agriprice/agriprice/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from core import views
 urlpatterns = [
+    path('',views.home),
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
 ]
