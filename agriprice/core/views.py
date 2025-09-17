2d6b5f3fc5d0b6142a910d69f11cd8d8c538c9a3
Nice-91 <iradukundanice91@gmail.com>
2025-09-17 00:41:38 -0700

    added changes

--- agriprice/core/views.py
+++ agriprice/core/views.py
@@ -104,7 +104,7 @@ class ProductViewSet(viewsets.ModelViewSet):
     filter_backends = [filters.SearchFilter]
     search_fields = ['name', 'description', 'season']
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+        serializer.save(owner=self.request.user.userprofile)
 class NotificationListView(generics.ListAPIView):
     serializer_class = NotificationSerializer
     permission_classes = [permissions.IsAuthenticated]
