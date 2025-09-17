d80036cd1cd7c5a8cab682f774a8d8246ca31672
Nice-91 <iradukundanice91@gmail.com>
2025-09-17 00:54:20 -0700

    added changes

--- agriprice/core/models.py
+++ agriprice/core/models.py
@@ -1,6 +1,15 @@
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.profile.save()
 
 class User(AbstractUser):
     email = models.EmailField(
