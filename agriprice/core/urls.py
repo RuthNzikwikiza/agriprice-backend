ab721a1fc1a2026eecff833abe02434beda2b185
Nice-91 <iradukundanice91@gmail.com>
2025-09-19 09:59:53 +0200

    added changes

--- agriprice/core/urls.py
+++ agriprice/core/urls.py
@@ -1,26 +1,25 @@
-# core/urls.py
-from . import views
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from . import views
 from .views import UserProfileViewSet, ProductViewSet
-
-
-urlpatterns = [
-    # Price Predictions
-    path('price-predictions/', views.PricePredictionListCreateView.as_view(), name='price-predictions-list-create'),
-    path('price-predictions/<int:pk>/', views.
-         PricePredictionRetrieveUpdateDeleteView.as_view(), name='price-predictions-rud'),
-
-    # Notifications
-    path('notifications/', views.NotificationListCreateView.as_view(), name='notifications-list-create'),
-    path('notifications/<int:pk>/', views.NotificationRetrieveUpdateDeleteView.as_view(), name='notifications-rud'),
-]
-
 
 router = DefaultRouter()
 router.register(r'profiles', UserProfileViewSet)
 router.register(r'products', ProductViewSet)
 
 urlpatterns = [
+    # Price Predictions
+    path('price-predictions/', views.PricePredictionListCreateView.as_view(),
+         name='price-predictions-list-create'),
+    path('price-predictions/<int:pk>/', views.PricePredictionRetrieveUpdateDeleteView.as_view(),
+         name='price-predictions-rud'),
+
+    # Notifications
+    path('notifications/', views.NotificationListCreateView.as_view(),
+         name='notifications-list-create'),
+    path('notifications/<int:pk>/', views.NotificationRetrieveUpdateDeleteView.as_view(),
+         name='notifications-rud'),
+
+    # Router endpoints (profiles, products)
     path('', include(router.urls)),
 ]
